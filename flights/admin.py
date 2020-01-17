d13cad22178e92b71427130ba54d5a013a73ba16
panu2306 <pranavbhendawade@gmail.com>
2020-01-17 20:21:36 +0530

    Admin Panel Customization using ModelAdmin class.

--- flights/admin.py
+++ flights/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Airport, Flight, Passenger
+
 # Register your models here.
-admin.site.register(Airport)
-admin.site.register(Flight)
+
+@admin.register(Airport)
+class AirportAdmin(admin.ModelAdmin):
+    list_display = ('city', 'code',)
+
+@admin.register(Flight)
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('origin', 'destination', 'duration')
+
 admin.site.register(Passenger)
